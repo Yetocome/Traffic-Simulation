9c3ebe7b2cea96eaacd12b14d5d87c074278ecc7
Yetocome <250745820@qq.com>
2017-01-24 06:24:07 +0800

    Basim simulation completed.

--- models.py
+++ models.py
@@ -23,6 +23,8 @@ import random
 from scipy import stats
 import numpy as np
 
+sim_clock = 0.1 # second
+
 vehicle_data = {
     # 'Type': (length, acceleration, deceleration, probability)
     'car': (4, 3, 4, 0.9),
@@ -47,16 +49,55 @@ speed_data = [
 
 # Emit vechiles with poisson distribution
 def poisson_distribution(flow):
-    return stats.poisson.rvs(mu=flow,loc=0,size=1)[0]
+    return 60*60/stats.poisson.rvs(mu=flow,loc=0,size=1)[0]
+
+def auto_or_not(ratio):
+    if random.random() > ratio:
+        return False
+    else:
+        return True
 
 def weighted_distribution(flow):
-    return flow
+    return 60*60/flow
 
-def lane_changing_model():
+def lane_changing_model(vehicle):
     pass
 
-def vehicle_following_model():
-    pass
+def vehicle_speedup_model(vehicle):
+    old_speed = vehicle.speed
+    vehicle.speed += vehicle.acc_rate*sim_clock
+    if vehicle.speed > vehicle.desired_speed:
+        vehicle.speed = vehicle.desired_speed
+
+    dis = (vehicle.speed+old_speed)*sim_clock/2
+    vehicle.front_location += dis
+    vehicle.back_location += dis
+
+def vehicle_slowdown_model(vehicle):
+    old_speed = vehicle.speed
+    vehicle.speed -= vehicle.dec_rate*sim_clock
+    if vehicle.speed < 0:
+        vehicle.speed = 0
+
+    dis = (vehicle.speed+old_speed)*sim_clock/2
+    vehicle.front_location += dis
+    vehicle.back_location += dis
+
+def vehicle_free_traveling_model(vehicle):
+    vehicle.front_location += vehicle.speed*sim_clock
+    vehicle.back_location += vehicle.speed*sim_clock
+
+def vehicle_following_model(leader, chaser):
+    safe_dis = chaser.front_location+chaser.get_safe_dis(type(leader) == type(chaser))
+    if leader.back_location > safe_dis+chaser.speed*sim_clock:
+        if chaser.speed < chaser.desired_speed:
+            vehicle_speedup_model(chaser)
+        else:
+            vehicle_free_traveling_model(chaser)
+    elif leader.back_location > safe_dis:
+        vehicle_slowdown_model(chaser)
+
+
 
 class Section(object):
     def __init__(self, miles, demand, lanes):
@@ -146,10 +187,11 @@ class Section(object):
 
 class Vehicle(object):
     def __init__(self):
-        self.speed = self.desired_speed()
+        self.desired_speed = self.desired_speed()
+        self.speed = self.desired_speed
         self.type = self.get_type()
         self.length, self.acc_rate, self.dec_rate, _ = vehicle_data[self.type]
-        self.time_gap = 1.4
+        self.manual_time_gap = 1.4
         self.front_location = 0
         self.back_location = self.front_location - self.length
 
@@ -164,53 +206,153 @@ class Vehicle(object):
         population = [val for val, cnt in weighted_choices for i in range(cnt)]
         return random.choice(population)
 
+    def get_safe_dis(self, Flag):
+        return self.manual_time_gap*self.speed
+
+    def set_location(self, back_location):
+        self.back_location = back_location
+        self.front_location = back_location+self.length
+
 class AutoVehicle(Vehicle):
     def __init__(self):
         Vehicle.__init__(self)
-        self.time_gap = 0.5
+        self.auto_time_gap = 0.5
+
+    def get_safe_dis(self, AutoFlag):
+        if AutoFlag:
+            return self.auto_time_gap*self.speed
+        else:
+            return self.manual_time_gap*self.speed
 
 # Define a basic simulator to derive specific simulator
 class BasicSimulator(object):
     def __init__(self):
-        self.time_slot = 0.1 # second
+
         self.curr_time = 0
+        self.countdown = [0, 0]
 
     def ticktock(self):
         pass
 
 class SingleLaneSimulator(BasicSimulator):
-    def __init__(self):
+    def __init__(self, hours):
         BasicSimulator.__init__(self)
-        self.time_limits = int(24*60*60/self.time_slot)
-        self.mainstream_pending_vehicles = []
-        self.ramp_waiting_vehicles = []
-        self.mainstream_vehicles = []
+        self.time_limits = int(hours*60*60/sim_clock)
+        # self.mainstream_pending_vehicles = []
+        # self.ramp_waiting_vehicles = []
+        # self.mainstream_vehicles = []
 
     def mainstream_simulation(self):
-        pass
+        if self.countdown[0] == 0:
+            if auto_or_not(self.auto_ratio):
+                self.mainstream_pending_vehicles.append(AutoVehicle())
+            else:
+                self.mainstream_pending_vehicles.append(Vehicle())
+            self.countdown[0] = int(poisson_distribution(self.single_lane_demand)/sim_clock)
+        else:
+            self.countdown[0] -= 1
+
+        # if self.mainstream_pending_vehicles != []:
+        #     vehicle = self.mainstream_pending_vehicles[0]
+        #     try:
+        #         vehicle_following_model(self.mainstream_vehicles[-1], vehicle)
+        #     except IndexError:
+        #         vehicle_free_traveling_model(vehicle)
+        try:
+            vehicle_free_traveling_model(self.mainstream_vehicles[0])
+        except IndexError:
+            pass
+        for i in range(self.mainstream_vehicles.__len__()-1):
+            vehicle_following_model(self.mainstream_vehicles[i], self.mainstream_vehicles[i+1])
+
+        if self.mainstream_pending_vehicles != []:
+            chaser = self.mainstream_pending_vehicles[0]
+            if self.mainstream_vehicles != []:
+                leader = self.mainstream_vehicles[-1]
+            # # check if the leader vehicle is in front of the safe distance of the joining one
+                if leader.back_location > chaser.get_safe_dis(type(leader) == type(chaser)):
+                    self.mainstream_vehicles.append(chaser)
+                    del self.mainstream_pending_vehicles[0]
+                    #     vehicle_free_traveling_model(vehicle)
+            else:
+                self.mainstream_vehicles.append(chaser)
+        if self.mainstream_vehicles != []:
+            if self.mainstream_vehicles[0].front_location > self.curr_sec.length:
+                del self.mainstream_vehicles[0]
 
     def ramp_simulation(self):
-        pass
+        if self.curr_sec.on_ramp_num > 0:
+            if self.countdown[1] == 0:
+                if auto_or_not(self.auto_ratio):
+                    self.ramp_waiting_vehicles.append(AutoVehicle())
+                else:
+                    self.ramp_waiting_vehicles.append(Vehicle())
+                self.countdown[1] = weighted_distribution(self.curr_sec.delta_demand/self.curr_sec.on_ramp_num/sim_clock)
+            else:
+                self.countdown[1] -= 1
+
+            try:
+                ramp_vehicle = self.ramp_waiting_vehicles[0]
+                back_location = 7/12*self.curr_sec.length
+                ramp_vehicle.set_location(back_location)
+                dash_into_flag = True
+                forbidden_area = (back_location+ramp_vehicle.length+ramp_vehicle.get_safe_dis(False), back_location-ramp_vehicle.get_safe_dis(False))
+
+                for i in range(self.mainstream_vehicles.__len__()-1):
+                    if self.mainstream_vehicles[i].back_location < forbidden_area[0] or self.mainstream_vehicles[i].front_location > forbidden_area[1]:
+                        dash_into_flag = False
+                        break
+                    elif self.mainstream_vehicles[i].back_location >= forbidden_area[0] and self.mainstream_vehicles[i+1].front_location <= forbidden_area[1]:
+                        self.mainstream_vehicles.insert(i, ramp_vehicle)
+                        del self.ramp_waiting_vehicles[0]
+                        dash_into_flag = False
+                # for only one vehicle situation
+                if dash_into_flag:
+                    self.mainstream_vehicles.append(ramp_vehicle)
+                    del self.ramp_waiting_vehicles[0]
+            except IndexError:
+                pass
+        else:
+            pass
+
+        if self.curr_sec.off_ramp_num > 0:
+            pass
+        else:
+            pass
 
     def ticktock(self):
         self.mainstream_simulation()
         self.ramp_simulation()
+        self.curr_time += 1
 
     def log(self):
-        pass
+        if self.curr_time%6000 == 0:
+            print(self.curr_time, self.mainstream_vehicles.__len__(), 'on the road',
+                self.mainstream_pending_vehicles.__len__(), 'pending to start',
+                self.ramp_waiting_vehicles.__len__(), 'waiting on the ramp',
+            )
 
     def congestion_detection(self):
         if self.ramp_waiting_vehicles.__len__() > self.curr_sec.main_cap or self.mainstream_pending_vehicles.__len__() > self.curr_sec.ramp_cap:
             return True
-        return False
+        # return False
 
-    def run(self, section_list):
+    def accident_detection(self):
+        pass
+
+    def run(self, section_list, ratio):
         self.list = section_list
+        self.auto_ratio = ratio
         for sec in self.list:
             self.curr_sec = sec
+            self.mainstream_pending_vehicles = []
+            self.ramp_waiting_vehicles = []
+            self.mainstream_vehicles = []
+            self.single_lane_demand = (self.curr_sec.demand-self.curr_sec.delta_demand)/self.curr_sec.lanes
             for i in range(self.time_limits):
                 self.ticktock()
                 self.log()
                 if self.congestion_detection():
                     self.curr_sec.record_time(self.curr_time)
+                    print('After', int(self.curr_time*sim_clock), 'seconds, congestion detected!!!')
                     break
--- simulation.py
+++ simulation.py
@@ -1,16 +1,23 @@
-import data, models, xlrd
+import data, models, xlrd, time
 
+
+start = time.clock()
 # data processing
 raw = xlrd.open_workbook('2017_MCM_Problem_C_Data.xlsx')
 data.process_data(raw)
 
-simulator = models.SingleLaneSimulator()
+simulator = models.SingleLaneSimulator(5) # set virtual hours in simulator
 
-# for i, v in data.IncRoads.items():
-#     simulator.run(v)
-#
-# for i, v in data.DecRoads.items():
-#     simulator.run(v)
+ratio = 0.5
+
+for i, v in data.IncRoads.items():
+    simulator.run(v, ratio)
+
+for i, v in data.DecRoads.items():
+    simulator.run(v, ratio)
 
 
-simulator.run([data.IncRoads[5][0]])
+# simulator.run([data.IncRoads[5][20]], 0.5)
+
+end = time.clock()
+print('It costs',end-start,'seconds.')
